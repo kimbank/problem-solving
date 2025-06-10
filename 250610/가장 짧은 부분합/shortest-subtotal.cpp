8d742737ae2c382adb9b72659498578bfee1e6e2
Eunhang Kim <87305109+kimbank@users.noreply.github.com>
2025-06-10 10:56:32 +0900

    Title: 가장 짧은 부분합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250610/가장 짧은 부분합/shortest-subtotal.cpp
+++ 250610/가장 짧은 부분합/shortest-subtotal.cpp
@@ -1,42 +1,32 @@
 #include <bits/stdc++.h>
-
-#define INF int(1e9)
-#define MAX_N 100001
-
 using namespace std;
 
-int n, s, ans = INF;
-int arr[MAX_N];
+int n, s;
+int arr[100001];
 
-int main(int argc, char* argv[]) {
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
     cin >> n >> s;
+    for (int i = 0; i < n; ++i)
+        cin >> arr[i];
 
-    for (int i = 1; i <= n; i++) {
-        int tmp;
-        cin >> tmp;
-        arr[i] = tmp;
-    }
+    int start = 0, end = 0, sum = 0;
+    int ans = INT_MAX;
 
-    for (int i = 1; i <= n; i++) {
-        int cur_sum = 0, j = i;
-
-        while (cur_sum < s && j <= n && j-i < ans) {
-            cur_sum += arr[j];
-            j++;
-        }
-
-        if (cur_sum >= s) {
-            ans = min(ans, j-i);
+    while (true) {
+        if (sum >= s) {
+            ans = min(ans, end - start);
+            sum -= arr[start++];
+        } else if (end == n) break;
+        else {
+            sum += arr[end++];
         }
     }
 
-    if (ans == INF) {
-        cout << -1 << "\n";
-        return 0;
-    }
-    cout << ans << "\n";
+    if (ans == INT_MAX) cout << -1 << "\n";
+    else cout << ans << "\n";
+
+    return 0;
 }
