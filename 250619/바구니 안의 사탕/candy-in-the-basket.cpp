b08900272e7e4e8aa60a92f49558ac887d665491
Eunhang Kim <87305109+kimbank@users.noreply.github.com>
2025-06-19 23:42:22 +0900

    Title: 바구니 안의 사탕, Time: 38ms, Memory: 4MB, Status: Wrong Answer - Codetree

---
+++ 250619/바구니 안의 사탕/candy-in-the-basket.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+#define MAX_ARR int(1e6)
+using namespace std;
+
+int n, k, ans;
+int max_idx;
+int arr[MAX_ARR];
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n >> k;
+
+    for (int i = 0 ; i < n; i++) {
+        int cnt, idx;
+        cin >> cnt >> idx;
+        arr[idx] += cnt;
+        max_idx = max(max_idx, idx);
+    }
+
+    int s;
+    int e = -1;
+    int acc{0};
+    int max_width = k * 2;
+    for (s = 0; s <= max_idx; s++) {
+        while (e + 1 < max_idx && e - s <= max_width) {
+            acc += arr[e];
+            e++;
+        }
+
+        ans = max(ans, acc);
+        acc -= arr[s];
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
