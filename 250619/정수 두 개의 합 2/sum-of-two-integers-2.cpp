fd1286969826e497a40498f9d4f43d6f9a899d13
Eunhang Kim <87305109+kimbank@users.noreply.github.com>
2025-06-19 21:53:09 +0900

    Title: 정수 두 개의 합 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250619/정수 두 개의 합 2/sum-of-two-integers-2.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+#define MAX_N 100001
+
+using namespace std;
+
+int n, k, ans;
+int arr[MAX_N];
+
+int main(void) {
+    cin >> n >> k;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+
+    int s = 1;
+    int e = 1;
+    int s_item = arr[s];
+    for (;s <= n; s++) {
+        s_item = arr[s];
+
+        while (e + 1 <= n && s_item + arr[e + 1] <= k) {
+            ans++;
+            e++;
+        }
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
