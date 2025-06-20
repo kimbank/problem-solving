f8bcea0b3a5e3968ca0f1b262fdd3f2dd4aad1b8
Eunhang Kim <87305109+kimbank@users.noreply.github.com>
2025-06-21 00:27:55 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250621/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+
+#define MAX_N int(1e5)
+
+using namespace std;
+
+int n, d;
+int ans = INT_MAX;
+vector<pair<int,int>> v;
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    cin >> n >> d;
+
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        v.push_back({x, y});
+        // v.push_back({y, x});
+    }
+
+    sort(v.begin(), v.end());
+
+    // int s_idx{0}, e_idx{n - 1};
+    // while (s_idx < e_idx) {
+    //     auto [s_y, s_x] = v[s_idx];
+    //     auto [e_y, e_x] = v[e_idx];
+
+    //     if (abs(s_y - e_y) < d) {
+
+    //     }
+
+    //     if (abs(s_y - e_y) >= d) {
+    //         ans = min(ans, abs(e_x - s_x));
+    //     }
+    // }
+
+    int s_idx{0}, e_idx{0};
+    for (s_idx = 0; s_idx < n - 1; s_idx++) {
+        if (s_idx >= e_idx)
+            e_idx = s_idx +1;
+        auto [s_x, s_y] = v[s_idx];
+        auto [e_x, e_y] = v[e_idx];
+
+        while (e_idx + 1 < n && abs(s_y - v[e_idx].second) < d) {
+            // auto [e_x, e_y] = v[e_idx + 1];
+            e_idx++;
+        }
+
+        if (abs(s_y - v[e_idx].second) >= d) {
+            ans = min(ans, abs(v[e_idx].first - s_x));
+        }
+    }
+
+    if (ans == INT_MAX) {
+        ans = -1;
+    }
+    cout << ans << "\n";
+
+    return 0;
+}
