842e1f7a2ed3adb6baf2389ced399eb3c68f8f34
Eunhang Kim <87305109+kimbank@users.noreply.github.com>
2025-06-23 19:46:42 +0900

    Title: 겹치는 원소가 적은 부분 수열, Time: 32ms, Memory: 2MB, Status: Wrong Answer - Codetree

---
+++ 250623/겹치는 원소가 적은 부분 수열/subsequences-with-few-overlapping-elements.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define MAX_N int(1e5)
+
+using namespace std;
+
+int n, k, ans, cnt;
+int arr[MAX_N];
+unordered_map<int, int> um;
+
+int main(void) {
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        um[arr[i]] = 0;
+    }
+
+    int s{0}, e{-1};
+    for (s = 0; s < n; s++) {
+        while (e + 1 < n && um[arr[e + 1]] < k) {
+            ans = max(ans, ++cnt);
+            um[arr[e + 1]]++;
+            e++;
+        }
+
+        if (arr[s] == 0) {
+            arr[s]--;
+            cnt--;
+        }
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
