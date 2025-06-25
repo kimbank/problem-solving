bb6f343adf2039f0b30169fcb7dda76a98699eb2
Eunhang Kim <87305109+kimbank@users.noreply.github.com>
2025-06-25 21:01:48 +0900

    Title: 부분수열 가능성 판단하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250625/부분수열 가능성 판단하기/determine-possibility-of-subsequence.cpp
+++ 250625/부분수열 가능성 판단하기/determine-possibility-of-subsequence.cpp
@@ -1,57 +1,45 @@
 #include <bits/stdc++.h>
-
-#define MAX_NM int(1e5)
-
 using namespace std;
 
-int n, m, ans;
-int a[MAX_NM], b[MAX_NM];
-
-int main(void) {
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n, m;
     cin >> n >> m;
+    vector<int> a(n), b(m);
 
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
+    for (int& x : a) cin >> x;
+    for (int& x : b) cin >> x;
+
+    vector<int> prefix(m, -1), suffix(m, -1);
+
+    // 앞에서부터 매칭
+    int j = 0;
+    for (int i = 0; i < n && j < m; i++) {
+        if (a[i] == b[j]) {
+            prefix[j] = i;
+            j++;
+        }
     }
 
+    // 뒤에서부터 매칭
+    j = m - 1;
+    for (int i = n - 1; i >= 0 && j >= 0; i--) {
+        if (a[i] == b[j]) {
+            suffix[j] = i;
+            j--;
+        }
+    }
+
+    int ans = 0;
     for (int i = 0; i < m; i++) {
-        cin >> b[i];
+        int left = (i == 0 ? -1 : prefix[i - 1]);
+        int right = (i == m - 1 ? n : suffix[i + 1]);
+        if (left < right) ans++;
     }
 
-    for (int i = 0; i < m; i++) {
-        vector<int> cur;
-        for (int j = 0; j < m; j++) {
-            if (j != i) {
-                cur.push_back(b[j]);
-            } 
-        }
-
-        vector<bool> visited(cur.size(), false);
-        int s{0}, e{0};
-        for (e = 0; e < cur.size(); e++) {
-            
-            while (s + 1 < n && a[s] != cur[e]) {
-                s++;
-            }
-            
-            if (a[s] == cur[e]) {
-                visited[e] = true;
-            }
-        }
-
-        ans++;
-        for (auto k : visited) {
-            if (!k) {
-                ans--;
-                break;
-            }
-        }
-    }
-
-    cout << ans << "\n";
-
+    cout << ans << '\n';
+    
     return 0;
 }
