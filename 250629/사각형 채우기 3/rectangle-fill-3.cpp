5d87c2f42c1adf8a3b443cbee253ae032d13ca3a
Eunhang Kim <87305109+kimbank@users.noreply.github.com>
2025-06-29 21:12:11 +0900

    Title: 사각형 채우기 3, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250629/사각형 채우기 3/rectangle-fill-3.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+#define MAX_N 1000
+#define MOD (unsigned long long)(1e9 + 7)
+
+using namespace std;
+
+int n, ans;
+unsigned long long dp[MAX_N + 1];
+
+int main(void) {
+    cin >> n;
+
+    dp[0] = 1UL;
+    dp[1] = 2UL;
+    dp[2] = 7UL;
+    for (int i = 3; i < n + 1; i++) {
+        dp[i] = ((2 * dp[i - 1]) + (4 * dp[i - 2])) % MOD;
+    }
+
+    cout << dp[n] << "\n";
+
+    return 0;
+}
