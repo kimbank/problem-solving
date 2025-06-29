f258f5a06299e4c98ca5c1ea96c71400f83fcb13
Eunhang Kim <87305109+kimbank@users.noreply.github.com>
2025-06-29 22:01:04 +0900

    Title: 사각형 채우기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250629/사각형 채우기 3/rectangle-fill-3.cpp
+++ 250629/사각형 채우기 3/rectangle-fill-3.cpp
@@ -1,24 +1,28 @@
 #include <bits/stdc++.h>
-
 #define MAX_N 1000
 #define MOD (unsigned long long)(1e9 + 7)
-
 using namespace std;
 
-int n, ans;
+int n;
 unsigned long long dp[MAX_N + 1];
+unsigned long long sum[MAX_N + 1];  // 장식 패턴 누적합
 
 int main(void) {
     cin >> n;
 
-    dp[0] = 1UL;
-    dp[1] = 2UL;
-    // dp[2] = 7UL;
-    for (int i = 2; i < n + 1; i++) {
-        dp[i] = ((2 * dp[i - 1]) + (((i % 2 == 0) ? 3 : 4) * dp[i - 2])) % MOD;
+    dp[0] = 1;
+    dp[1] = 2;
+    dp[2] = 7;
+    sum[0] = dp[0];
+    sum[1] = (dp[0] + dp[1]) % MOD;
+    sum[2] = (sum[1] + dp[2]) % MOD;
+
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (2 * dp[i - 1] + 3 * dp[i - 2] + 2 * sum[i - 3]) % MOD;
+        sum[i] = (sum[i - 1] + dp[i]) % MOD;
     }
 
     cout << dp[n] << "\n";
-
+    
     return 0;
 }
