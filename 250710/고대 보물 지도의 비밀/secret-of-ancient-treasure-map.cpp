ed23ea43956cfb68023978a1fd34d5574568023b
Eunhang Kim <87305109+kimbank@users.noreply.github.com>
2025-07-10 15:07:36 +0900

    Title: 고대 보물 지도의 비밀, Time: 12ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250710/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.cpp
+++ 250710/고대 보물 지도의 비밀/secret-of-ancient-treasure-map.cpp
@@ -1,43 +1,39 @@
 #include <bits/stdc++.h>
-
-#define MAX_N 100001
-
 using namespace std;
 
-int n, k;
-long long ans = LLONG_MIN;
+const int MAX_N = 1e5 + 1;
 int arr[MAX_N];
 
-int main(void) {
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+    int n, k;
     cin >> n >> k;
 
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    int left{0}, right{0}, cur_k{0};
-    long long cur_sum{0};
-    for (; right < n; right++) {
-        cur_sum += arr[right];
-        if (arr[right] < 0) {
-            cur_k++;
-        }
+    int left = 0, negative_cnt = 0;
+    long long sum = 0, max_sum = LLONG_MIN;
 
-        while (left <= right && cur_k > k) {
-            cur_sum -= arr[left];
-            if (arr[left] < 0) {
-                cur_k--;
-            }
+    for(int right = 0; right < n; right++) {
+        // 오른쪽 포인터의 원소 추가
+        sum += arr[right];
+        if(arr[right] < 0) negative_cnt++;
+
+        // 음수가 k개를 초과하면, 왼쪽 포인터 이동
+        while(negative_cnt > k && left <= right) {
+            if(arr[left] < 0) negative_cnt--;
+            sum -= arr[left];
             left++;
         }
 
-        ans = max(ans, cur_sum);
+        // 조건 만족 시 최대 합 갱신
+        max_sum = max(max_sum, sum);
     }
 
-    cout << ans << "\n";
-
+    cout << max_sum << "\n";
     return 0;
 }
