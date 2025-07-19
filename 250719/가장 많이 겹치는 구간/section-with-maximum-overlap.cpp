1852e88be58542e38dc681dd6137ed5cb9e93698
Eunhang Kim <87305109+kimbank@users.noreply.github.com>
2025-07-19 10:19:52 +0900

    Title: 가장 많이 겹치는 구간, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250719/가장 많이 겹치는 구간/section-with-maximum-overlap.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define MAX_N int(1e5 + 1)
+
+using namespace std;
+
+int n, s, e, ans;
+int points[MAX_N];
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int x1, x2;
+        cin >> x1 >> x2;
+
+        s = min(s, x1);
+        e = max(e, x2);
+
+        points[x1]++;
+        points[x2]--;
+    }
+
+    int acc{0};
+    for (int i = s; i <= e; i++) {
+        acc += points[i];
+
+        ans = max(ans, acc);
+    }
+
+    cout << ans << "\n";
+
+    return 0;
+}
