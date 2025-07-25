178f90b7750abdd7029b582345194cc540330583
Eunhang Kim <87305109+kimbank@users.noreply.github.com>
2025-07-25 13:59:35 +0900

    Title: 인접하지 않은 3개의 숫자, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250725/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.cpp
+++ 250725/인접하지 않은 3개의 숫자/three-non-adjacent-numbers.cpp
@@ -16,10 +16,10 @@ int main() {
         cin >> arr[i];
     }
 
-    dp[0][1] = arr[0];
-    dp[2][2] = arr[2];
+    dp[0][1] = arr[0]; dp[1][1] = arr[0];
+    dp[2][2] = arr[2]; dp[3][2] = arr[2];
     dp[4][3] = arr[4];
-    for (int i = 1; i < n; i++) {
+    for (int i = 2; i < n; i++) {
         dp[i][1] = max(dp[i-1][1], arr[i]);
         dp[i][2] = max(dp[i-1][2], dp[i-2][1] + arr[i]);
         dp[i][3] = max(dp[i-1][3], dp[i-2][2] + arr[i]);
