917f444e75208b2c24d0054bc7a3522dfa513b17
Eunhang Kim <87305109+kimbank@users.noreply.github.com>
2025-07-29 13:06:29 +0900

    Title: 씨 오 더블유, Time: 9ms, Memory: 1MB, Status: Passed - Codetree

--- 250729/씨 오 더블유/c-o-w.cpp
+++ 250729/씨 오 더블유/c-o-w.cpp
@@ -1,20 +1,62 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
+
+#define MAX_N int(1e5+1)
+
 using namespace std;
 
+int n;
+long long ans;
+string word;
+
 int main() {
-    int n;
-    string word;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     cin >> n >> word;
 
-    long long cntC = 0, cntO = 0, cntW = 0;
-
-    for (char ch : word) {
-        if (ch == 'C') cntC++;
-        else if (ch == 'O') cntO += cntC;
-        else if (ch == 'W') cntW += cntO;
+    /** Solution 1: Codetree */
+    // L: 'C'
+    int L[MAX_N];
+    L[0] = 0;
+    for (int i = 0; i < n; i++) {
+        if (word[i] == 'C') {
+            L[i + 1] = L[i] + 1;
+        } else {
+            L[i + 1] = L[i];
+        }
     }
 
-    cout << cntW << "\n";
+    // R: 'W'
+    int R[MAX_N];
+    R[n] = 0;
+    for (int i = n - 1; i > 0; i--) {
+        if (word[i] == 'W') {
+            R[i] = R[i + 1] + 1;
+        } else {
+            R[i] = R[i + 1];
+        }
+    }
+
+    // find ans
+    for (int i = 0; i <= n; i++) {
+        // cout << "L: " << L[i] << " R:" << R[i] << endl;
+        if (word[i] == 'O') {
+            ans += L[i + 1] * R[i + 1];
+        }
+    }
+
+    cout << ans << "\n";
+
+    /** Solution 2 */
+    // long long cntC = 0, cntO = 0, cntW = 0;
+
+    // for (char ch : word) {
+    //     if (ch == 'C') cntC++;
+    //     else if (ch == 'O') cntO += cntC;
+    //     else if (ch == 'W') cntW += cntO;
+    // }
+
+    // cout << cntW << "\n";
+
     return 0;
 }
